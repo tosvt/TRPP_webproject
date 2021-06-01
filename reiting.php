3f1236bd5457c7d64ae4b4ee5ad68fa0923da498
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-01 10:09:21 +0300

    Update reiting.php

--- reiting.php
+++ reiting.php
@@ -1,28 +1,11 @@
-<?php session_start(); include "functions.php"; include "connect.php";
-// if( $_SESSION['USER_LOGIN_IN'] == 1){
-// echo  $_SESSION['login'];
-// }elseif ($_SESSION['USER_COOKIE_IN'] == 1) echo $_COOKIE['user'];
-// else echo "вы никак не подключены";
-
-function getGrpByLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT grp FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['grp'];
-        closeDB($mysqli);
-    }
-$grp = getGrpByLogin($_SESSION['login']);
-
-function getFioOnLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT name FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['name'];
-        closeDB($mysqli);
-    }
-$name = getFioOnLogin($_SESSION['login']);
-$show = getALlProjects($row);
-   ?>
+<?php session_start(); require_once 'connect.php';  
+$login = $_SESSION['login'];
+$grpandname = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
+while($user = mysqli_fetch_assoc($grpandname)) {
+    $name = $user['name']; 
+    $grp = $user['grp']; 
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -38,21 +21,17 @@ $show = getALlProjects($row);
        <h1>Рейтинг участников:</h1>
         <section class="section">
             <?php 
-                    
-                 $link = mysqli_connect($host, $user, $password, $db);
-                 $sql = "SELECT SUM(ozenka_experta) AS `sum`, COUNT(ozenka_experta) AS `count`, id_uchastnika, name_uchastnik, ozenka_experta FROM ozenki GROUP BY name_uchastnik HAVING COUNT(ozenka_experta)>1";
-                 $sql1 = "SELECT SUM(ozenka_experta)  AS `sum`, COUNT(ozenka_experta) AS `count`, id_uchastnika, name_uchastnik FROM ozenki GROUP BY id_uchastnika, name_uchastnik ORDER BY sum DESC";
-                 $result = mysqli_query($link, $sql1);
-                while($row = mysqli_fetch_assoc($result)){
-                    echo '<div class="reit">
-                            <p>'.$row['name_uchastnik'].' — '.round($row['sum']/$row['count'],2).'</p> 
-                            </div>' ;
-                }
-               
+				        $sql1 = mysqli_query($connect, "SELECT SUM(ozenka_experta) AS `sum`, COUNT(ozenka_experta) AS `count`, id_uchastnika, name_uchastnik, ozenka_experta FROM ozenki GROUP BY name_uchastnik HAVING COUNT(ozenka_experta)>1");
+                $sql2 = mysqli_query($connect, "SELECT SUM(ozenka_experta)  AS `sum`, COUNT(ozenka_experta) AS `count`, id_uchastnika, name_uchastnik FROM ozenki GROUP BY id_uchastnika, name_uchastnik ORDER BY sum DESC");  
+				        while ($result = mysqli_fetch_array($sql1)) {
+					             echo '<div class="reit">
+                            <p>'.$result['name_uchastnik'].' — '.round($result['sum']/$result['count'],2).'</p> 
+                            </div>';
+				}
             ?>
         </section>
        
     </main>
-    <footer> &copy; 2021 Открытй конкурс Интернет-проектов <br> Разработка сайта: Золотухин С.А., Коротыч Г.Д., Шелюхин В.П.</footer>
+    <footer> &copy; 2021 Открытый конкурс Интернет-проектов <br> Разработка сайта: Золотухин С.А., Коротыч Г.Д., Шелюхин В.П.</footer>
 </body>
 </html>

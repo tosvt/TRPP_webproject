f22129db9a7584f7f400609025c698eac8b188c4
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-01 10:09:53 +0300

    Update vvod_ozenki.php

--- vvod_ozenki.php
+++ vvod_ozenki.php
@@ -1,54 +1,18 @@
-<?php session_start(); include "functions.php"; include "connect.php";
-// if( $_SESSION['USER_LOGIN_IN'] == 1){
-// echo  $_SESSION['login'];
-// }elseif ($_SESSION['USER_COOKIE_IN'] == 1) echo $_COOKIE['user'];
-// else echo "вы никак не подключены";
+<?php session_start(); require_once 'connect.php';
 $login = $_SESSION['login'];
-function getGrpByLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT grp FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['grp'];
-        closeDB($mysqli);
-    }
-$grp = getGrpByLogin($_SESSION['login']);
-
-function getFioOnLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT name FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['name'];
-        closeDB($mysqli);
-    }
-$name = getFioOnLogin($_SESSION['login']);
-
-
-$show = getALlProjects($row, $_GET['id']);
-
-
-function getIDByLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT id FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['id'];
-        closeDB($mysqli);
-    }
-
-    function getNameByIdExpert($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT name FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['name'];
-        closeDB($mysqli);
-    }
-
+$grpandname = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
+while($user = mysqli_fetch_assoc($grpandname)) {
+    $name = $user['name']; 
+    $grp = $user['grp'];
+    $id = $user['id']; 
+}
    ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ПР ИБ</title>
+    <title>Выставление оценки</title>
     <link rel="stylesheet" href="main.css">
 
 </head>
@@ -56,18 +20,21 @@ function getIDByLogin($login){
     <main>
       <?php include "header.php";?>
         <section>
-            <h1>Выставление оценок:</h1>
+            <h1>Выставление оценки:</h1>
             <?php 
-        
-                
-                    echo '<div class="works"><fieldset>
-                           <legend>#'.$show["id"].'</legend>
-                           <p><b>Автор: </b>'.$show["name"].'</p>
-                           <p><b>Название: </b>'.$show["title"].'</p>
-                           <p><b>Описание: </b>'.$show["decription"].'</p>
-                          
-                        </fieldset></div>' ;
-            
+            $id_work = $_GET['id'];
+            $work = mysqli_query($connect, "SELECT * FROM `works` WHERE `id` = '$id_work'");
+            while($res = mysqli_fetch_assoc($work)) {
+              $id_uchastnika = $res['id'];
+              $name_uchastnik = $res['name'];
+                echo '<div class="works">
+                        <fieldset>
+                            <legend>#'.$id_uchastnika.'</legend>
+                            <p><b>Автор: </b>'.$name_uchastnik.'</p>
+                            <p><b>Название: </b>'.$res["title"].'</p>
+                            <p><b>Описание: </b>'.$res["description"].'</p>
+                        </fieldset></div>';
+            }
             ?>
             <div class="container">
                 <h3>Отзыв о работе: </h3><br>
@@ -87,30 +54,21 @@ function getIDByLogin($login){
                     <input type="submit" name="send_otzyv"  class="btn" value="Оценить проект">
                 </form>
                 <?php 
-                    
-                    $id_raboti = $show['id'];
-                    $id_uchastnika = $show['id_login']; 
-                    $id_experta = getIDByLogin($login);
-                    $name_expert = getNameByIdExpert($login);
-                    $name_uchastnik = $show['name'];
                     $ozenka_experta = $_POST['ozenka'];
                     $otzyv_experta = $_POST['otzyv'];
                     
-                if(isset($_POST['send_otzyv'])) {
+                    if(isset($_POST['send_otzyv'])) {
                      
-                           $mysqli = connectDB();
-                         $mysqli->query("INSERT INTO `ozenki` (`id_raboti`,`id_uchastnika`, `name_uchastnik`, `id_experta`, `nam_expert`, `otzyv_experta`, `ozenka_experta`) VALUES ('$id_raboti', '$id_uchastnika', '$name_uchastnik', '$id_experta', '$name_expert', '$otzyv_experta', '$ozenka_experta')");
-                       if($mysqli) echo "<div style='text-align: center; color:green; '>Ваш отзыв успешно добавлен!</div>";
+                        $sendreview = mysqli_query($connect, "INSERT INTO `ozenki` (`id_raboti`,`id_uchastnika`, `name_uchastnik`, `id_experta`, `nam_expert`, `otzyv_experta`, `ozenka_experta`) VALUES ('$id_work', '$id_uchastnika', '$name_uchastnik', '$id', '$name', '$otzyv_experta', '$ozenka_experta')");
+                       if($sendreview) echo "<div style='text-align: center; color:green; '>Ваш отзыв успешно добавлен!</div>";
                        else echo "<div style='text-align: center; color:red; '>К сожалению, Ваш отзыв не был добавлен!</div>";
-                            closeDB($mysqli);
-               
-                }
+                   }
             
                 ?>
             </div>
         </section>
        
     </main>
-    <footer> &copy; 2021 Открытй конкурс Интернет-проектов <br> Разработка сайта: Золотухин С.А., Коротыч Г.Д., Шелюхин В.П.</footer>
+    <footer> &copy; 2021 Открытый конкурс Интернет-проектов <br> Разработка сайта: Золотухин С.А., Коротыч Г.Д., Шелюхин В.П.</footer>
 </body>
 </html>

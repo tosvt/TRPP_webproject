00cae36b4b2cb3f45830898e3425339fcf73bf0d
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-01 10:01:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
-<?php session_start(); include "functions.php"; include "connect.php";
-
-function getGrpByLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT grp FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['grp'];
-        closeDB($mysqli);
-    }
-$grp = getGrpByLogin($_SESSION['login']);
-
-function getFioOnLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT name FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['name'];
-        closeDB($mysqli);
-    }
-$name = getFioOnLogin($_SESSION['login']);
-
-$show = getALlProjects($row);
-
-   ?>
+<?php session_start(); require_once 'connect.php';
+$login = $_SESSION['login'];
+$grpandname = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
+while($user = mysqli_fetch_assoc($grpandname)) {
+    $name = $user['name']; 
+    $grp = $user['grp']; 
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,18 +20,15 @@ $show = getALlProjects($row);
         <h1>Работы участников:</h1>
         <section>
             <?php 
-    
-                $mysqli = new mysqli($host, $user, $password, $db);
-             
-                for($i = 0; $i<count($show); $i++) {
-                    echo '<div class="works"><fieldset>
-                           <legend>#'.$show[$i]["id"].'</legend>
-                           <p><b>Автор: </b>'.$show[$i]["name"].'</p>
-                           <p><b>Название: </b>'.$show[$i]["title"].'</p>
-                           <p><b>Описание: </b>'.$show[$i]["decription"].'</p>
-                        </fieldset></div>' ;
-                }
-                closeDB($mysqli);
+				$works = mysqli_query($connect, "SELECT * FROM `works`");
+				while ($result = mysqli_fetch_array($works)) {
+					echo '<div class="works"><fieldset>
+                           <legend>#'.$result["id"].'</legend>
+                           <p><b>Автор: </b>'.$result["name"].'</p>
+                           <p><b>Название: </b>'.$result["title"].'</p>
+                           <p><b>Описание: </b>'.$result["description"].'</p>
+                        </fieldset></div>';
+				}
             ?>
          
         </section>

14fe215033b99e433dd5704a2918b2cd76a30b32
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-01 10:04:13 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,5 @@
 	session_start();
 	unset($_SESSION['login']);
 	unset($_SESSION['password']);
-	setcookie("user", "", time() - 3600);
-	header("Location: ".$_SERVER['HTTP_REFERER']);
+	header("Location: /");
 ?>

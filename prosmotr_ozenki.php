cc6310ccf6d398eddacebd69518e26dc6656a834
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-01 10:06:59 +0300

    Update prosmotr_ozenki.php

--- prosmotr_ozenki.php
+++ prosmotr_ozenki.php
@@ -1,37 +1,12 @@
-<?php session_start(); include "functions.php"; include "connect.php";
-
-function getGrpByLogin($login){
-    $mysqli = connectDB();
-    $result = $mysqli->query("SELECT grp FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['grp'];
-        closeDB($mysqli);
-    }
-$grp = getGrpByLogin($_SESSION['login']);
-
-function getFioOnLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT name FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['name'];
-        closeDB($mysqli);
-    }
-$name = getFioOnLogin($_SESSION['login']);
-
-$show = getALlProjects($row, $_GET['id']);
-
+<?php session_start(); require_once 'connect.php'; 
 $login = $_SESSION['login'];
-
-function getIDByLogin($login){
-    $mysqli = connectDB();
-    $result = $mysqli->query("SELECT id FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['id'];
-        closeDB($mysqli);
-    }
- 
-    $showc = getComments($row, $_GET['id']);
- 
+$grpandname = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
+while($user = mysqli_fetch_assoc($grpandname)) {
+    $name = $user['name']; 
+    $grp = $user['grp'];
+    $id = $user['id']; 
+}
+ $id_work = $_GET['id'];
    ?>
 <!DOCTYPE html>
 <html>
@@ -45,45 +20,42 @@ function getIDByLogin($login){
     <main>
       <?php include "header.php";?>
         <section>
-            <h1>Проект #<?php echo $show['id'];?></h1>
+            <h1>Проект #<?php echo $id_work;?></h1>
             <?php 
        
-                
-                    echo '<div class="works"><fieldset>
-                           <legend>#'.$show["id"].'</legend>
-                           <p><b>Автор: </b>'.$show["name"].'</p>
-                           <p><b>Название: </b>'.$show["title"].'</p>
-                           <p><b>Описание: </b>'.$show["decription"].'</p>
-                        </fieldset></div>' ;
+              $work = mysqli_query($connect, "SELECT * FROM `works` WHERE `id` = '$id_work'");
+              while($res = mysqli_fetch_assoc($work)) {
+                echo '<div class="works">
+                        <fieldset>
+                            <legend>#'.$res["id"].'</legend>
+                            <p><b>Автор: </b>'.$res["name"].'</p>
+                            <p><b>Название: </b>'.$res["title"].'</p>
+                            <p><b>Описание: </b>'.$res["description"].'</p>
+                        </fieldset></div>';
+            }
             ?>
             <div class="container">
                 <h3>Отзывы: </h3><br>
                 
                 <?php 
-                    
-            
-                $id = $_GET['id'];
-                $link = mysqli_connect($host, $user,$password, $db);
-             
-                $sql = "SELECT * FROM ozenki WHERE id_raboti = '$id'";
-                $result = mysqli_query($link, $sql);
-                $row_cnt = $result->num_rows;
-                if($row_cnt == 0) echo "<h3>Еще нет комментариев для этого проекта.</h3>";
-                while($row = mysqli_fetch_assoc($result)){
-                    echo '<div class="reviews">
-                           
-                           <p><b>Написал: </b>'.$row['nam_expert'].'</p>
-                           <p><b>Оценка: </b>'.$row["ozenka_experta"].'</p>
-                           <p><b>Комментарий: </b>'.$row["otzyv_experta"].'</p>
-                     </div> ' ;
+                $work = mysqli_query($connect, "SELECT * FROM ozenki WHERE id_raboti = '$id_work'");
+                $allworkcomments = mysqli_fetch_all($work);
+                if(mysqli_num_rows($work) > 0){
+                    foreach($allworkcomments as $comment) {
+                        echo '<div class="reviews">
+                           <p><b>Написал: </b>'.$comment[5].'</p>
+                           <p><b>Оценка: </b>'.$comment[7].'</p>
+                           <p><b>Комментарий: </b>'.$comment[6].'</p>
+                       </div> ';        
+                    }
+                } else {
+                    echo "<h3>Еще нет комментариев для этого проекта.</h3>";
                 }
-               
-
                 ?>
             </div>
         </section>
        
     </main>
-    <footer> &copy; 2021 Открытй конкурс Интернет-проектов <br> Разработка сайта: Золотухин С.А., Коротыч Г.Д., Шелюхин В.П.</footer>
+    <footer> &copy; 2021 Открытый конкурс Интернет-проектов <br> Разработка сайта: Золотухин С.А., Коротыч Г.Д., Шелюхин В.П.</footer>
 </body>
 </html>

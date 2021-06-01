a9710323da3df84f0dc6f997596105bb7c59ad39
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-01 10:05:19 +0300

    Update ozenki.php

--- ozenki.php
+++ ozenki.php
@@ -1,24 +1,12 @@
-<?php session_start(); include "functions.php"; include "connect.php";
-
-function getGrpByLogin($login){
-    $mysqli = connectDB();
-    $result = $mysqli->query("SELECT grp FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['grp'];
-        closeDB($mysqli);
-    }
-$grp = getGrpByLogin($_SESSION['login']);
-
-function getFioOnLogin($login){
-       $mysqli = connectDB();
-    $result = $mysqli->query("SELECT name FROM users WHERE login = '$login'");
-    $row = $result->fetch_assoc();
-    return $row['name'];
-        closeDB($mysqli);
-    }
-$name = getFioOnLogin($_SESSION['login']);
-$show = getALlProjects($row, $_GET['id']);
-
+<?php session_start(); require_once 'connect.php';
+$login = $_SESSION['login'];
+$grpandname = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
+while($user = mysqli_fetch_assoc($grpandname)) {
+    $name = $user['name']; 
+    $grp = $user['grp']; 
+    $id = $user['id'];
+}
+echo "$id";
    ?>
 <!DOCTYPE html>
 <html>
@@ -35,19 +23,18 @@ $show = getALlProjects($row, $_GET['id']);
        <h1>Просмотр оценок:</h1>
         <section class="viewmarks">
             <?php 
-       
-                $mysqli = new mysqli($host, $user, $password, $db);
-             
-                for($i = 0; $i<count($show); $i++) {
-                    echo '<div class="works"><fieldset>
-                           <legend>#'.$show[$i]["id"].'</legend>
-                           <p><b>Автор: </b>'.$show[$i]["name"].'</p>
-                           <p><b>Название: </b>'.$show[$i]["title"].'</p>
-                           <p><b>Описание: </b>'.$show[$i]["decription"].'</p>
-                           <p align="right"><a href="/prosmotr_ozenki.php?id='.$show[$i]["id"].'"><i>Смотреть оценку эксперта</i></a></p>
-                        </fieldset></div>' ;
-                }
-                closeDB($mysqli);
+            $worksbyid = mysqli_query($connect, "SELECT * FROM `works` WHERE `id_login` = '$id'");
+            $allworksbyid = mysqli_fetch_all($worksbyid);
+            foreach($allworksbyid as $allworks) {
+                echo '<div class="works">
+                        <fieldset>
+                            <legend>#'.$allworks[0].'</legend>
+                            <p><b>Автор: </b>'.$allworks[1].'</p>
+                            <p><b>Название: </b>'.$allworks[2].'</p>
+                            <p><b>Описание: </b>'.$allworks[3].'</p>
+                            <p align="right"><a href="/prosmotr_ozenki.php?id='.$allworks[0].'"><i>Смотреть оценку эксперта</i></a></p>
+                        </fieldset></div>';        
+            }
             ?>
          
         </section>
